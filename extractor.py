350fb2412b0a0785cb2799817f76d5584e3c6708
NedNedov228 <nakhmedli@gmail.com>
2023-02-21 11:36:43 +0400

    Moved page extractor to a function

--- extractor.py
+++ extractor.py
@@ -2,29 +2,39 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-id = 3030965
-url = f'https://bina.az/items/{id}'
 
-r = requests.get(url)
+keys,vals,prices,priceCurs = list(),list(),list(),list()
+locations = ""
 
-soup = BeautifulSoup(r.text, 'lxml')
+def pageDataExtract(id):
+    global locations
 
-trs = soup.find("table", {"class": "parameters"}).findAll('tr')
-keys,vals = list(),list()
-for i in trs:
-    keys.append((i.findAll("td"))[0].text)
-    vals.append((i.findAll("td"))[1].text)
+    url = f'https://bina.az/items/{id}'
+
+    r = requests.get(url)
+
+    soup = BeautifulSoup(r.text, 'lxml')
+
+    trs = soup.find("table", {"class": "parameters"}).findAll('tr')
+
+    for i in trs:
+        keys.append((i.findAll("td"))[0].text)
+        vals.append((i.findAll("td"))[1].text)
+
+
+    prices.append(int(soup.find('span', {"class":"price-val"}).text.replace(" ", "")))
+    priceCurs.append(soup.find('span', {"class":"price-cur"}).text)
+
+
+    loclist = soup.find("ul",{"class":"locations"}).findAll("a")
+
+    for i in loclist:locations+=i.text+","
+
 
 info = dict(zip(keys,vals))
-
-price = {'price' : int(soup.find('span', {"class":"price-val"}).text.replace(" ", "")) , "price-cur" : soup.find('span', {"class":"price-cur"}).text}
-info.update(price)
-
-loclist = soup.find("ul",{"class":"locations"}).findAll("a")
-locations = ""
-for i in loclist:locations+=i.text+","
+info.update({"price":prices,"price-cur":priceCurs})
 info.update({"locations" : locations})
 
-info_df = pd.DataFrame([info])
+info_df = pd.DataFrame(info)
 
 info_df.to_csv('data.csv', sep=' ', index=False)
