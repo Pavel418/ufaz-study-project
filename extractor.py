27442b6a3f806b386b552a9e5d086ee47e746271
Pavel <pavel@vvv.az>
2023-03-12 18:54:36 +0400

    new commit

--- extractor.py
+++ extractor.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-keys,vals,prices,priceCurs = list(),list(),list(),list()
+keys,vals,prices,priceCurs, latitudes, longitudes = list(),list(),list(),list(), list(),list()
 locations = ""
 
 def pageDataExtract(id):
@@ -14,27 +14,34 @@ def pageDataExtract(id):
     r = requests.get(url)
 
     soup = BeautifulSoup(r.text, 'lxml')
-
-    trs = soup.find("table", {"class": "parameters"}).findAll('tr')
-
+    try:
+        trs = soup.find("table", {"class": "parameters"}).findAll('tr')
+    except Exception as e:
+        return
     for i in trs:
         keys.append((i.findAll("td"))[0].text)
         vals.append((i.findAll("td"))[1].text)
-
-
     prices.append(int(soup.find('span', {"class":"price-val"}).text.replace(" ", "")))
     priceCurs.append(soup.find('span', {"class":"price-cur"}).text)
-
-
+    try:
+        latitudes.append(soup.find('div', id='agency_map').get('data-lat'))
+        longitudes.append(soup.find('div', id='agency_map').get('data-lng'))
+    except Exception as e:
+        latitudes.append(soup.find('div', id='item_map').get('data-lat'))
+        longitudes.append(soup.find('div', id='item_map').get('data-lng'))
+        
     loclist = soup.find("ul",{"class":"locations"}).findAll("a")
 
     for i in loclist:locations+=i.text+","
-
-
+for i in range(3279157, 3279157 + 10):
+    print(i)
+pageDataExtract(3279159)
+#pageDataExtract(3142195)
 info = dict(zip(keys,vals))
 info.update({"price":prices,"price-cur":priceCurs})
 info.update({"locations" : locations})
+info.update({"latitude": latitudes, 'longtitude': longitudes})
 
 info_df = pd.DataFrame(info)
 
-info_df.to_csv('data.csv', sep=' ', index=False)
+info_df.to_csv('data.csv')
