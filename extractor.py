f792e42d4aeab3e1c125b6218dc2b894d65a3a86
Pavel418 <pavel@vvv.az>
2023-03-15 19:43:57 +0400

    optimization

--- extractor.py
+++ extractor.py
@@ -1,48 +1,69 @@
 import requests
 from bs4 import BeautifulSoup
+import datetime
 import pandas as pd
 import numpy as np
+import multiprocessing as mp
 
-
-keys,vals,area,cixaris, ipoteka,rooms,repairs,prices,priceCurs, latitudes, longitudes = list(),list(),list(),list(), list(),list(),list(),list(),list(),list(),list()
-locations = ""
-
-def pageDataExtract(id):
-    global locations
-
-    url = f'https://bina.az/items/{id}'
-
-    r = requests.get(url)
-
-    soup = BeautifulSoup(r.text, 'lxml')
+def pageDataExtract(id, session):
     try:
-        trs = soup.find("table", {"class": "parameters"}).findAll('tr')
+        url = f'https://bina.az/items/{id}'
+        r = session.get(url)
+        if "Tapılmadı" in r.text:
+            return
+
+        locations = ""
+        info = {}
+        fields = {"Kateqoriya", "Mərtəbə", "Sahə", "Otaq sayı","Çıxarış","Təmir", "İpoteka"}
+
+        soup = BeautifulSoup(r.text, 'lxml')
+        if "Alış" != soup.find('div', {"class":"search-row__cell search-row__cell--leased"}).find('option', {"selected": "selected"}).text:
+            return
+        trs = soup.find("table", {"class": "parameters"}).find_all('tr')
+        for i in trs:
+            key = (i.find_all("td"))[0].text
+            if key in fields:
+                val = i.find_all("td")[1].text
+                if key == "Kateqoriya":
+                    if val != "Köhnə tikili" and val != "Yeni tikili":
+                        return
+                info.update({key: val})
+                fields.remove(key)
+        for i in fields:
+            info.update({key: np.NaN})
+        city = soup.find('div', {"class":"search-row__cell search-row__cell--city"}).find('option', {"selected": "selected"}).text
+        price = int(soup.find('span', {"class":"price-val"}).text.replace(" ", ""))
+        priceCur = soup.find('span', {"class":"price-cur"}).text
+        try:
+            latitude = soup.find('div', id='agency_map').get('data-lat')
+            longitude = soup.find('div', id='agency_map').get('data-lng')
+        except Exception as e:
+            latitude = soup.find('div', id='item_map').get('data-lat')
+            longitude = soup.find('div', id='item_map').get('data-lng')
+            
+        loclist = soup.find("ul",{"class":"locations"}).find_all("a")
+
+        locations = ','.join([tag.text for tag in loclist])
+        
+        info.update({"price":price,"price-cur":priceCur,
+                    "locations" : locations,"latitude": latitude,
+                    "city":city, 'longtitude': longitude, "id": id})
+        return info
     except Exception as e:
         return
-    for i in trs:
-        keys.append((i.findAll("td"))[0].text)
-        vals.append((i.findAll("td"))[1].text)
-        print((i.findAll("td"))[0].text)
-    prices.append(int(soup.find('span', {"class":"price-val"}).text.replace(" ", "")))
-    priceCurs.append(soup.find('span', {"class":"price-cur"}).text)
-    try:
-        latitudes.append(soup.find('div', id='agency_map').get('data-lat'))
-        longitudes.append(soup.find('div', id='agency_map').get('data-lng'))
-    except Exception as e:
-        latitudes.append(soup.find('div', id='item_map').get('data-lat'))
-        longitudes.append(soup.find('div', id='item_map').get('data-lng'))
-        
-    loclist = soup.find("ul",{"class":"locations"}).findAll("a")
 
-    for i in loclist:locations+=i.text+","
-for i in range(3279157, 3279157 + 1000, 100):
-    pageDataExtract(i)
-#pageDataExtract(3142195)
-info = dict(zip(keys,vals))
-info.update({"price":prices,"price-cur":priceCurs})
-info.update({"locations" : locations})
-info.update({"latitude": latitudes, 'longtitude': longitudes})
+if __name__ == "__main__":
+    session = requests.Session()
+    rows= []
+    begin = datetime.datetime.now()
+    with mp.Pool() as pool:
+        results = pool.starmap(pageDataExtract, [(i, session) for i in range(2000000, 3500000, 868)])
+        for row in results:
+            if row is not None:
+                rows.append(row)
+    #rows.append(pageDataExtract(3142195, session))
+    end = datetime.datetime.now()
+    print((end - begin).seconds)
+    info_df = pd.DataFrame(rows)
 
-info_df = pd.DataFrame(info)
-
-info_df.to_csv('data.csv')
+    info_df.to_csv('data.csv')
